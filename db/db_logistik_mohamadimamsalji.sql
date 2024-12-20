45589ebd7b469b980a3c1bebc96fbe06eb873b48
imamsalji <imamsalji7@gmail.com>
2024-12-20 15:39:04 +0700

    Update db

--- db/db_logistik_mohamadimamsalji.sql
+++ db/db_logistik_mohamadimamsalji.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 20, 2024 at 09:01 AM
+-- Generation Time: Dec 20, 2024 at 09:38 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.1.25
 
@@ -45,7 +45,37 @@ INSERT INTO `barang` (`kode_barang`, `nama_barang`, `deskripsi_barang`, `stok`, 
 (2, 'Printer Epson', 'Printer Epson multifungsi dengan kemampuan cetak warna dan hitam putih.', 0, NULL, '2024-12-19 15:39:17'),
 (3, 'Monitor LG', 'Monitor LG dengan resolusi Full HD, ideal untuk produktivitas dan hiburan.', 35, NULL, '2024-12-20 06:26:42'),
 (4, 'Keyboard Logitech', 'Keyboard Logitech ergonomis dengan fitur wireless untuk kenyamanan mengetik.', 100, NULL, NULL),
-(5, 'Mouse Razer', 'Mouse gaming Razer dengan sensor presisi tinggi dan desain ergonomis.', 96, NULL, '2024-12-20 07:32:37');
+(5, 'Mouse Razer', 'Mouse gaming Razer dengan sensor presisi tinggi dan desain ergonomis.', 96, NULL, '2024-12-20 07:32:37'),
+(6, 'Laptop', 'Laptop dengan spek tinggi', 10, NULL, NULL),
+(7, 'Kulkas', 'Kulkas 2 pintu', 5, NULL, NULL),
+(8, 'Smartphone', 'Smartphone dengan kamera 108 MP', 15, NULL, NULL),
+(9, 'Televisi', 'Televisi LED 50 inci', 8, NULL, NULL),
+(10, 'Kamera', 'Kamera mirrorless dengan lensa kit', 3, NULL, NULL),
+(11, 'AC', 'AC 1 PK', 6, NULL, NULL),
+(12, 'Microwave', 'Microwave dengan 20 liter kapasitas', 12, NULL, NULL),
+(13, 'Mesin Cuci', 'Mesin cuci 1 tabung', 7, NULL, NULL),
+(14, 'Blender', 'Blender 3 in 1', 20, NULL, NULL),
+(15, 'Rice Cooker', 'Rice cooker 1 liter', 5, NULL, NULL),
+(16, 'Fan', 'Kipas angin berdiri', 25, NULL, NULL),
+(17, 'Jam Tangan', 'Jam tangan digital', 30, NULL, NULL),
+(18, 'Mouse', 'Mouse wireless', 18, NULL, NULL),
+(19, 'Keyboard', 'Keyboard mekanik', 14, NULL, NULL),
+(20, 'Speaker', 'Speaker bluetooth', 10, NULL, NULL),
+(21, 'Power Bank', 'Power bank 10000mAh', 50, NULL, NULL),
+(22, 'Tas Ransel', 'Tas ransel multifungsi', 22, NULL, NULL),
+(23, 'Sepatu', 'Sepatu olahraga', 28, NULL, NULL),
+(24, 'Baju', 'Baju kaos lengan panjang', 33, NULL, NULL),
+(25, 'Celana', 'Celana panjang jeans', 19, NULL, NULL),
+(26, 'Sandal', 'Sandal casual', 25, NULL, NULL),
+(27, 'Kacamata', 'Kacamata hitam', 15, NULL, NULL),
+(28, 'Payung', 'Payung lipat', 50, NULL, NULL),
+(29, 'Sarung Tangan', 'Sarung tangan winter', 40, NULL, NULL),
+(30, 'Dompet', 'Dompet kulit', 15, NULL, '2024-12-20 08:30:34'),
+(31, 'Parfum', 'Parfum pria 50ml', 20, NULL, NULL),
+(32, 'Shampoo', 'Shampoo anti ketombe', 50, NULL, NULL),
+(33, 'Sabun', 'Sabun mandi wangi', 45, NULL, NULL),
+(34, 'Lilin', 'Lilin aromaterapi', 60, NULL, NULL),
+(35, 'Peralatan Dapur', 'Peralatan dapur set lengkap', 15, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -69,7 +99,8 @@ CREATE TABLE `barang_keluar` (
 --
 
 INSERT INTO `barang_keluar` (`id_barang_keluar`, `no_barang_keluar`, `kode_barang`, `quantity`, `destination`, `tanggal_keluar`, `created_at`, `updated_at`) VALUES
-(1, '40', 2, 30, 'ciawi', '2024-12-12', '2024-12-19 15:39:17', '2024-12-19 15:39:17');
+(1, '40', 2, 30, 'ciawi', '2024-12-12', '2024-12-19 15:39:17', '2024-12-19 15:39:17'),
+(3, 'D001', 30, 20, 'ciawi', '2024-12-27', '2024-12-20 08:30:34', '2024-12-20 08:30:34');
 
 -- --------------------------------------------------------
 
@@ -255,13 +286,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `kode_barang` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `kode_barang` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT for table `barang_keluar`
 --
 ALTER TABLE `barang_keluar`
-  MODIFY `id_barang_keluar` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_barang_keluar` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `barang_masuk`
